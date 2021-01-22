04ba583d8616d0d4ebdb529a5e0bf08100a820cd
Marcel Rusu <vincntrosy@gmail.com>
2021-01-22 17:14:12 -0500

    get scrolling col w cards working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Page from './misc/Page';
+import Card from './misc/Card';
 
 const Col = ({children}) => (
-  <div className="mx-5 bg-gray-300 rounded w-1/3 h-full">
+  <div className="mx-5 mb-1 bg-gray-300 rounded w-1/3 h-full overflow-scroll py-2" style={{boxShadow: 'inset #00000040 0px 1px 3px 1px'}}>
     {children}
   </div>
 )
+
+const cards = Array.from({length: 7}).map((_, i) => ({
+  title: `Item #${i + 1}`,
+  content: 'Has to finish this issue tracker'
+}));
 
 function App() {
   return (
@@ -15,7 +21,13 @@ function App() {
         <a>Done</a>
       </header>  
       <div className="flex h-full">
-        <Col/>
+        <Col>
+          {cards.map(({title, content}) => (
+            <Card className="m-2" title={title}>
+              <p>{content}</p>
+            </Card>
+          ))}
+        </Col>
         <Col/>
         <Col/>
       </div>
--- src/misc/Card.js
+++ src/misc/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './Card.scss';
 
-export default ({title, children}) => (
-  <div className="card">
+export default ({title, children, className}) => (
+  <div className={`card ${className}`}>
     <div className="header">
       <h3>{title}</h3>
     </div>
