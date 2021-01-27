c95e55738e55b7982f28bcaf59bcf7f0322be74b
Marcel Rusu <vincntrosy@gmail.com>
2021-01-26 20:04:03 -0500

    fix top alignment

--- src/misc/Overlay.js
+++ src/misc/Overlay.js
@@ -7,7 +7,7 @@ const Overlay = ({children, invisible, onClickOut}) => (
       onClick={onClickOut}
       className={`absolute z-10 w-screen h-screen ${!invisible && 'bg-gray-500 opacity-30'} left-0 top-0`}
     />
-    <div className={`absolute w-1/5 z-20 top-1/5 overlay-content`}>
+    <div className={`absolute w-1/5 z-20 top-11 overlay-content`}>
       {children}
     </div>
   </>
